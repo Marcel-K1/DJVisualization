631a42f438c201cd5143f4904e6bc4ba5fdec74c
Marcel Klein <Marcel-K1@gmx.net>
2023-02-23 18:46:44 +0100

    Update

--- Assets/Scripts/AudioVisualization.cs
+++ Assets/Scripts/AudioVisualization.cs
@@ -6,46 +6,65 @@ using UnityEngine.Video;
 
 public class AudioVisualization : MonoBehaviour
 {
+    [Header("Background")]
     [SerializeField]
     private AudioData audioData = null;
     [SerializeField]
-    private Material materialToChange;
-    [SerializeField]
-    private VideoPlayer videoPlayerToChange;
-    [SerializeField]
-    private RawImage imageToChange;
-    [SerializeField]
-    private float colorPower = 1f;
-    [SerializeField]
-    private float playbackSpeed = 1f;
-    [SerializeField]
-    private float videoUpdateStep = 0.5f;
+    private Material materialToChange;   
     [SerializeField]
     private float backgroundUpdateStep = 0.1f;
-
     private float currentBackgroundUpdateTime = 0f;
-    private float currentVideoUpdateTime = 0f;
-    private Color colorValue = Color.white;
 
+    //[Header("Video Player")]
+    //[SerializeField]
+    //private VideoPlayer videoPlayerToChange;
+    //[SerializeField]
+    //private RawImage imageToChange;
+    //[SerializeField]
+    //private float colorPower = 1f;
+    //[SerializeField]
+    //private float playbackSpeed = 1f;
+    //[SerializeField]
+    //private float videoUpdateStep = 0.5f;
+    //private float currentVideoUpdateTime = 0f;
+
+    [Header("Image Player")]
+    [SerializeField]
+    private Texture[] imageArray = null;
+    [SerializeField]
+    private RawImage imageToChange = null;
+    //[SerializeField]
+    //int screenWidth = 1500;
+    //[SerializeField]
+    //int screenHeight = 1500;
+    [SerializeField]
+    private int currentImage = 0;
+
+
+    private Color colorFromAudioData = Color.white;
+    private float targetTime;
+
+
+    //private void OnGUI()
+    //{
+    //    Rect imageRect = new Rect(-(screenWidth-(screenWidth/2)), -(screenHeight-(screenHeight/2)), screenWidth, screenHeight);
+    //    Debug.Log($"{imageRect.x},{imageRect.y}");
+    //    GUI.DrawTexture(imageRect, imageArray[currentImage]);
+    //    GUI.color = colorFromAudioData;
+    //}
 
     void Start()
     {
-        videoPlayerToChange.Prepare();
-        materialToChange.SetFloat("LavaSpeed", 1.5f);
+        //videoPlayerToChange.Prepare();
+        targetTime = 5;
     }
 
     void Update()
     {
-        ////AudioPeer
-        //colorValue = new Color(AudioPeer.samples[(int)Random.Range(0f, 128f)] * colorPower, AudioPeer.samples[(int)Random.Range(128f, 256f)] * colorPower, AudioPeer.samples[(int)Random.Range(256f, 512f)] * colorPower); ; ;
-        //if (materialToChange != null)
-        //{
-        //    materialToChange.SetColor("BaseColor", colorValue);
-        //}
+        //playbackSpeed = videoPlayerToChange.playbackSpeed;
+        //currentVideoUpdateTime += Time.deltaTime;
 
-        //AudioData
-        playbackSpeed = videoPlayerToChange.playbackSpeed;
-        currentVideoUpdateTime += Time.deltaTime;
+        //Background
         currentBackgroundUpdateTime += Time.deltaTime;
 
         if (currentBackgroundUpdateTime >= backgroundUpdateStep)
@@ -57,19 +76,37 @@ public class AudioVisualization : MonoBehaviour
                 float factor1 = Random.Range(0, 255);
                 float factor2 = Random.Range(0, 255);
                 float factor3 = Random.Range(0, 255);
-                float factor4 = Random.Range(2, 8);
-                float factor5 = Random.Range(0, 255);
-                materialToChange.SetColor("BaseColor", new Color(audioData.ClipLoudness * factor1, audioData.ClipLoudness * factor2, audioData.ClipLoudness * factor3));
+                float factor4 = Random.Range(5f, 8f);
+                float randomfloat = Random.Range(0.01f, 0.1f);
+                colorFromAudioData = new Color(randomfloat * factor1, randomfloat * factor2, randomfloat * factor3);
+                
+                //Changing Background Properties
+                materialToChange.SetColor("BaseColor", colorFromAudioData);
                 materialToChange.SetFloat("LavaPower", factor4);
-                imageToChange.color = new Color(audioData.ClipLoudness * factor1, audioData.ClipLoudness * factor2, audioData.ClipLoudness * factor3, audioData.ClipLoudness * factor5);
+
+                //Changing Image Properties
+                //imagetoChange.color = colorFromAudioData;
             }
 
-            if (currentVideoUpdateTime >= videoUpdateStep)
-            {
-                currentVideoUpdateTime = 0f;
-                float factor1 = Random.Range(0.2f, 3f);
-                videoPlayerToChange.playbackSpeed = factor1;
-            }
+            //if (currentVideoUpdateTime >= videoUpdateStep)
+            //{
+            //    currentVideoUpdateTime = 0f;
+            //    float factor1 = Random.Range(0.2f, 3f);
+            //    videoPlayerToChange.playbackSpeed = factor1;
+            //}
         }
+
+        //Image Player
+        targetTime -= Time.deltaTime * 5;
+
+        if (targetTime <= 0)
+        {
+            currentImage = Random.Range(0, imageArray.Length);
+            targetTime = Random.Range(0, 5);
+            imageToChange.texture = imageArray[currentImage];
+        }
+
+
+
     }
 }
